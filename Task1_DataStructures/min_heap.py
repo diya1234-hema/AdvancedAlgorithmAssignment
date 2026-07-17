402e6817164b75f67119476c76a2a3713d902348
diya-neupane <439diyaneupane@gmail.com>
2026-07-17 15:26:30 +0545

    Implement Min heap priority queue

---
+++ Task1_DataStructures/min_heap.py
@@ -0,0 +1,27 @@
+import heapq
+
+
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+
+    def insert(self, distance, city):
+        heapq.heappush(
+            self.heap,
+            (distance, city)
+        )
+    def get_next_city(self):
+        return heapq.heappop(self.heap)
+heap = MinHeap()
+
+heap.insert(200, "Pokhara")
+heap.insert(0, "Kathmandu")
+heap.insert(250, "Butwal")
+
+print("Heap Contents:")
+print(heap.heap)
+
+next_city = heap.get_next_city()
+
+print("\nNext City to Visit:")
+print(next_city)
